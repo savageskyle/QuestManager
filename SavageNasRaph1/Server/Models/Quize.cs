b7e92eca706d6fd87af01462b2cc409e9bf88098
Jraaf <nasibovrafael1@gmail.com>
2023-06-15 21:11:27 +0300

    added model

---
+++ SavageNasRaph1/Server/Models/Quize.cs
@@ -0,0 +1,12 @@
+﻿namespace SavageNasRaph1.Server.Models
+{
+    public class Quize
+    {
+        public List<Question> Questions { get; set; }
+    }
+    public class Question
+    {
+        public string Text { get; set; }
+        public List<string> Answers { get; set; }
+    }
+}
